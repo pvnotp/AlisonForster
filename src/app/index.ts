dfeb6e68dec333d82cd0f1c88fd66d0b2406de39
Alison <alisonjoyforster@gmail.com>
2025-08-08 13:48:59 -0400

    Updating app index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,10 @@
 // created from 'create-ts-index'
 
-export * from './falling-blocks';
+
 export * from './app.component.spec';
 export * from './app.component';
 export * from './app.config';
 export * from './app.routes';
+
+export * from './falling-blocks';
+export * from './resume';
