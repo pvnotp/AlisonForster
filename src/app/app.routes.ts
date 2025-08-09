cbc6af54274eb7d57edd79ab83fa1d1881c5a1ad
Alison <alisonjoyforster@gmail.com>
2025-08-09 18:13:21 -0400

    Initial commit for about component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { FallingBlocksComponent } from './falling-blocks/falling-blocks.componen
 import { ResumeComponent } from './resume/resume.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 export const routes: Routes = [
   { path: 'resume', component: ResumeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', component: HomeComponent },
   { path: '**', component: FallingBlocksComponent, pathMatch: 'full' }
 ]
