b5c71dd377a66721ba997e110086d1810473db1d
Alison <alisonjoyforster@gmail.com>
2025-08-18 15:55:23 -0400

    Adding The GeoCities Times to projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,10 +11,18 @@ import { FallingBlocksComponent } from '../falling-blocks';
 })
 export class ProjectsComponent {
   public readonly alchemy: BlockData[] = [
-    { text: 'ALCHEMY COACHING', width: 1.25, linkText: 'VISIT', link: 'https://alchemycoaching-gacve7fsgmarengw.eastus2-01.azurewebsites.net/' },
+    { text: 'COACHING', width: 0.8, linkText: 'GITHUB', link: 'https://github.com/pvnotp/AlchemyCoaching' },
+    { text: 'ALCHEMY', width: 0.6, linkText: 'VISIT', link: 'https://alchemycoaching-gacve7fsgmarengw.eastus2-01.azurewebsites.net/' },
   ];
 
   public readonly alison: BlockData[] = [
-    { text: 'THIS WEBSITE', width: 1, linkText: 'HOME', link: '/' },
+    { text: 'WEBSITE', width: 0.6, linkText: 'GITHUB', link: 'https://github.com/pvnotp/AlisonForster' },
+    { text: 'THIS', width: 0.4, linkText: 'HOME', link: '/' },
+  ];
+
+  public readonly geocities: BlockData[] = [
+    { text: 'TIMES', width: 0.5, linkText: 'GITHUB', link: 'https://github.com/pvnotp/GeocitiesTimes'},
+    { text: 'GEOCITIES', width: 0.9, linkText: 'VISIT', link: 'https://geocitiestimes-h2daaffnehapbhav.eastus2-01.azurewebsites.net/' },
+    { text: 'THE', width: 0.3 },
   ];
 }
