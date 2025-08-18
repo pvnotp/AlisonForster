4c60937dd91f8c6628ceebc7780f1fdd4d4c62fe
Alison <alisonjoyforster@gmail.com>
2025-08-18 15:56:38 -0400

    Slight change to projects block sizes.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,7 +12,7 @@ import { FallingBlocksComponent } from '../falling-blocks';
 export class ProjectsComponent {
   public readonly alchemy: BlockData[] = [
     { text: 'COACHING', width: 0.8, linkText: 'GITHUB', link: 'https://github.com/pvnotp/AlchemyCoaching' },
-    { text: 'ALCHEMY', width: 0.6, linkText: 'VISIT', link: 'https://alchemycoaching-gacve7fsgmarengw.eastus2-01.azurewebsites.net/' },
+    { text: 'ALCHEMY', width: 0.7, linkText: 'VISIT', link: 'https://alchemycoaching-gacve7fsgmarengw.eastus2-01.azurewebsites.net/' },
   ];
 
   public readonly alison: BlockData[] = [
@@ -22,7 +22,7 @@ export class ProjectsComponent {
 
   public readonly geocities: BlockData[] = [
     { text: 'TIMES', width: 0.5, linkText: 'GITHUB', link: 'https://github.com/pvnotp/GeocitiesTimes'},
-    { text: 'GEOCITIES', width: 0.9, linkText: 'VISIT', link: 'https://geocitiestimes-h2daaffnehapbhav.eastus2-01.azurewebsites.net/' },
+    { text: 'GEOCITIES', width: 0.8, linkText: 'VISIT', link: 'https://geocitiestimes-h2daaffnehapbhav.eastus2-01.azurewebsites.net/' },
     { text: 'THE', width: 0.3 },
   ];
 }
